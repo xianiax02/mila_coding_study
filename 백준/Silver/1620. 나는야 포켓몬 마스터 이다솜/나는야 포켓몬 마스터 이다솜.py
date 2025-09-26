d4247a03a6cf458892ce33dfb90a80bf643a22c7
xianiax02 <102227649+xianiax02@users.noreply.github.com>
2025-09-26 11:01:00 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 240 ms, Memory: 47720 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -0,0 +1,17 @@
+import sys
+numdict=[]
+namedict={}
+n,m=list(map(int,(sys.stdin.readline().strip().split(' '))))
+
+for i in range(1,n+1):
+    data=sys.stdin.readline().strip()
+    numdict.append(data)
+    namedict[data]=i
+
+for j in range(m):
+    data=sys.stdin.readline().strip()
+    try:
+        datanum=int(data)
+        sys.stdout.write(numdict[datanum-1]+'\n')
+    except ValueError:
+        sys.stdout.write(str(namedict[data])+'\n')
