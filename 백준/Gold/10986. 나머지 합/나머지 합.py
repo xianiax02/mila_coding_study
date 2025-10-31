a7c01537cf4122e2389a87825ab7968669d4b9bf
xianiax02 <102227649+xianiax02@users.noreply.github.com>
2025-10-31 21:34:13 +0900

    [Gold III] Title: 나머지 합, Time: 804 ms, Memory: 144556 KB -BaekjoonHub

---
+++ 백준/Gold/10986. 나머지 합/나머지 합.py
@@ -0,0 +1,16 @@
+import sys
+input=sys.stdin.readline
+n,m=map(int,input().strip().split())
+numbers=[0]+[int(x) for x in input().strip().split()]
+r=[0 for _ in range(n+1)]
+cnt=0
+remains=[0 for _ in range(m)]
+for i in range(1,n+1):
+    r[i]=((r[i-1]%m)+(numbers[i]%m))%m
+    remains[r[i]]+=1
+remains[0]+=1
+for remainnum in remains:
+    cnt+=remainnum*(remainnum-1)//2
+
+
+sys.stdout.write(str(cnt))
