4c6b3697c72a99953e70aff50007d09e6a3b1a4e
xianiax02 <102227649+xianiax02@users.noreply.github.com>
2025-11-24 20:45:39 +0900

    [Silver III] Title: N과 M (1), Time: 132 ms, Memory: 34924 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,14 +1,14 @@
 import sys
 from collections import deque
-#input=sys.stdin.readline
-#print=sys.stdout.write
+input=sys.stdin.readline
+print=sys.stdout.write
 ms=deque()
 n,m=map(int,input().strip().split())
 visited=[False]*(n+1) #1 base indexing
 def DFS(m,n,depth):
     global ms
     if depth==m+1:
-        print(*ms)
+        print(' '.join(map(str,ms))+'\n')
         return
     for i in range(1,n+1):
         if not visited[i]:
