e2ca320ff674ea11fffecc7f7fba82421f8b8c2e
xianiax02 <102227649+xianiax02@users.noreply.github.com>
2025-12-31 17:35:32 +0900

    [Silver III] Title: 소수 구하기, Time: 676 ms, Memory: 79504 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -4,7 +4,7 @@ start,end=map(int,input().split())
 prime=list(range(2,end+1))
 
 prime=[True]*(end-1)
-for num in range(2,end): #끝을 제곱근으로 바꿔도 될거같은데
+for num in range(2,int(end**(1/2))+1): #끝을 제곱근으로 바꿔도 될거같은데
     if prime[num-2]:
         a=2
         while num*a<=end:
