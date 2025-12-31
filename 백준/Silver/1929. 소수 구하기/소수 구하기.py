324bf9b88c24c06564a56a1c45198ee790446b18
xianiax02 <102227649+xianiax02@users.noreply.github.com>
2025-12-31 17:33:37 +0900

    [Silver III] Title: 소수 구하기, Time: 1012 ms, Memory: 79504 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,18 @@
+start,end=map(int,input().split())
+
+
+prime=list(range(2,end+1))
+
+prime=[True]*(end-1)
+for num in range(2,end): #끝을 제곱근으로 바꿔도 될거같은데
+    if prime[num-2]:
+        a=2
+        while num*a<=end:
+            prime[num*a-2]=False
+            a+=1
+
+for num in range(start,end+1):
+    if num==1: #엣지케이스
+        continue
+    if prime[num-2]:
+        print(num)
