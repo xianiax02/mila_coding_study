4f7b1996ff5a98e32d43b6865659e5882cfa1852
xianiax02 <102227649+xianiax02@users.noreply.github.com>
2026-01-16 15:57:17 +0900

    [Silver III] Title: 1로 만들기, Time: 456 ms, Memory: 40224 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -2,6 +2,7 @@ n=int(input())
 inf=float("inf")
 f=[inf]*(1+n)
 f[1]=0
+'''
 for i in range(1,n+1):
     if 3*i<=n:
         f[3*i]=min(f[i]+1,f[3*i])
@@ -9,4 +10,12 @@ for i in range(1,n+1):
         f[2*i]=min(f[i]+1,f[2*i])
     if i+1<=n:
         f[i+1]=min(f[i]+1,f[i+1])
+print(f[n])
+'''
+for i in range(2,n+1):
+    f[i]=f[i-1]+1
+    if not i%2:
+        f[i]=min(f[i//2]+1,f[i])
+    if not i%3:
+        f[i]=min(f[i//3]+1,f[i])
 print(f[n])
