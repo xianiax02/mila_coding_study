08271d2e1c696c37a50bb1edf212f8ead1f00113
xianiax02 <102227649+xianiax02@users.noreply.github.com>
2026-02-01 16:09:30 +0900

    [level 2] Title: 타겟 넘버, Time: 262.36 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,14 +1,21 @@
 def solution(numbers, target):
-    n=len(numbers)
-    dp=[0]*(1<<n) #n+1자리수가 됨.
-    for i in range(n):
-        num=numbers[i]
-        temp=dp[:]
-        for index in range(1<<i):
-            value=temp[index]
-            leftindex=(index<<1)
-            rightindex=(index<<1)+1
-            dp[leftindex]=value-num
-            dp[rightindex]=value+num
-    answer = dp.count(target)
-    return answer
+    n = len(numbers)
+    dp = [0] * (1 << n)
+    
+    # 첫 번째 숫자는 미리 세팅 (0번 숫자를 처리한 상태로 시작)
+    dp[0] = -numbers[0]
+    dp[1] = numbers[0]
+    
+    # 1번째 숫자부터 n-1번째 숫자까지 처리
+    for i in range(1, n):
+        num = numbers[i]
+        # 현재 층의 마지막 노드 인덱스부터 0까지 거꾸로 진행
+        # i=1일 때 j는 1, 0 순서 / i=2일 때 j는 3, 2, 1, 0 순서
+        for j in range((1 << i) - 1, -1, -1):
+            value = dp[j] # 아직 업데이트되지 않은 부모의 값
+            
+            # 자식 인덱스는 부모 인덱스보다 크거나 같으므로 안전함
+            dp[j * 2] = value - num
+            dp[j * 2 + 1] = value + num
+            
+    return dp.count(target)
