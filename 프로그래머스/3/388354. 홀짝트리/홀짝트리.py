4dd16835fb4f335d6a5a16a18c53d16688e30fc9
XianXian <102227649+xianiax02@users.noreply.github.com>
2026-02-02 18:07:54 +0900

    [level 3] Title: 홀짝트리, Time: 1665.49 ms, Memory: 94.6 MB -BaekjoonHub

--- 프로그래머스/3/388354. 홀짝트리/홀짝트리.py
+++ 프로그래머스/3/388354. 홀짝트리/홀짝트리.py
@@ -1,37 +1,47 @@
-import sys
-sys.setrecursionlimit(10**6)
 def solution(nodes, edges):
-    treelist=[]
-    def dfs(v,l):
-        for nxt in adlist[v]:
-            if not visited[nxt]:
-                visited[nxt]=True
-                l.append(nxt)
-                dfs(nxt,l)
-    hnum=0 #type 0
-    rnum=0 #type 1
-    typelist=[0 for _ in range(max(nodes)+1)]
-    visited=[False]*(max(nodes)+1)
-    #make adlist
-    adlist=[[] for _ in range(max(nodes)+1)]
-    for edge in edges:
-        a,b=edge
-        adlist[a].append(b)
-        adlist[b].append(a)
+    parent = {node: node for node in nodes}
+    degree = {node: 0 for node in nodes}
+    
+    # 1. 차수 계산 및 Union-Find 준비
+    for a, b in edges:
+        degree[a] += 1
+        degree[b] += 1
+
+    def find(i):
+        if parent[i] == i:
+            return i
+        parent[i] = find(parent[i]) # 경로 압축 (Path Compression)
+        return parent[i]
+
+    def union(i, j):
+        root_i = find(i)
+        root_j = find(j)
+        if root_i != root_j:
+            parent[root_i] = root_j
+
+    # 2. 모든 간선을 합치기 (덩어리 나누기)
+    for a, b in edges:
+        union(a, b)
+
+    # 3. 루트별로 타입 개수 집계
+    # 결과 구조: {root_node: [type0_count, type1_count]}
+    comp_stats = {}
+    
     for node in nodes:
-        if node%2!=len(adlist[node])%2:
-            typelist[node]=1
-        if not visited[node]:
-            treelist.append([node])
-            visited[node]=True
-            dfs(node,treelist[-1])
-    for tree in treelist:
-        treetypes=[]
-        for node in tree:
-            treetypes.append(typelist[node])
-        if treetypes.count(0)==1:
-            hnum+=1
-        if treetypes.count(1)==1:
-            rnum+=1
-    answer = [hnum,rnum]
-    return answer
+        root = find(node)
+        if root not in comp_stats:
+            comp_stats[root] = [0, 0]
+        
+        # 타입 판별 로직 (v % 2 == degree % 2 이면 Type 0)
+        if node % 2 == degree[node] % 2:
+            comp_stats[root][0] += 1 # Type 0 추가
+        else:
+            comp_stats[root][1] += 1 # Type 1 추가
+
+    # 4. 최종 트리 개수 카운트
+    h_tree, r_tree = 0, 0
+    for t0, t1 in comp_stats.values():
+        if t0 == 1: h_tree += 1
+        if t1 == 1: r_tree += 1
+
+    return [h_tree, r_tree]
