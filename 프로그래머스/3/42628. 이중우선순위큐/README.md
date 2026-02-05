d215b10dd6b3dcfdc23190c119a3d86e7c27d90f
XianXian <102227649+xianiax02@users.noreply.github.com>
2026-02-05 16:59:49 +0900

    [level 3] Title: 이중우선순위큐, Time: 101.17 ms, Memory: 21.1 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/README.md
+++ 프로그래머스/3/42628. 이중우선순위큐/README.md
@@ -1,10 +1,10 @@
 # [level 3] 이중우선순위큐 - 42628 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42628) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42628#) 
 
 ### 성능 요약
 
-메모리: 15.6 MB, 시간: 143.87 ms
+메모리: 21.1 MB, 시간: 101.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 05일 16:10:57
+2026년 02월 05일 16:59:48
 
 ### 문제 설명
 
--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,22 +1,38 @@
-from bisect import bisect_left
+import heapq
+
 def solution(operations):
-    answer = []
-    for operation in operations:
-        command,num=operation.split()
-        num=int(num)
-        if command=='I':
-            if answer:
-                pos=bisect_left(answer,num)
-                answer.insert(pos,num)
-            else:
-                answer.append(num)
-        elif command=='D':
-            if answer:
-                if num==1:
-                    answer.remove(answer[-1])
-                elif num==-1:
-                    answer.remove(answer[0])
-    if not answer:
-        answer=[0,0]
-    else: answer=[answer[-1],answer[0]]
-    return answer
+    max_h, min_h = [], []
+    # 각 연산 인덱스별로 '생존 여부'를 체크하는 배열
+    visited = [False] * len(operations)
+    
+    for i, op in enumerate(operations):
+        cmd, val = op.split()
+        num = int(val)
+        
+        if cmd == 'I':
+            heapq.heappush(min_h, (num, i))   # (값, 고유ID)
+            heapq.heappush(max_h, (-num, i)) # (-값, 고유ID)
+            visited[i] = True               # i번 데이터 생존!
+            
+        elif num == 1: # 최댓값 삭제
+            # 1. 쓰레기 데이터 정리 (이미 다른 쪽에서 삭제된 놈들)
+            while max_h and not visited[max_h[0][1]]:
+                heapq.heappop(max_h)
+            # 2. 진짜 삭제
+            if max_h:
+                visited[max_h[0][1]] = False
+                heapq.heappop(max_h)
+        else: # 최솟값 삭제
+            while min_h and not visited[min_h[0][1]]:
+                heapq.heappop(min_h)
+            if min_h:
+                visited[min_h[0][1]] = False
+                heapq.heappop(min_h)
+
+    # 마지막 결과 출력 전에도 한 번 더 청소
+    while min_h and not visited[min_h[0][1]]: heapq.heappop(min_h)
+    while max_h and not visited[max_h[0][1]]: heapq.heappop(max_h)
+
+    if not min_h:
+        return [0, 0]
+    return [-max_h[0][0], min_h[0][0]]
