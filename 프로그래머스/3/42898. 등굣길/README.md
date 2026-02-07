10acdafa63602f3685e3ac9e23c235c80dfed545
XianXian <102227649+xianiax02@users.noreply.github.com>
2026-02-07 11:31:48 +0900

    [level 3] Title: 등굣길, Time: 4.28 ms, Memory: 9.45 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/README.md
+++ 프로그래머스/3/42898. 등굣길/README.md
@@ -1,10 +1,10 @@
 # [level 3] 등굣길 - 42898 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898#) 
 
 ### 성능 요약
 
-메모리: 9.51 MB, 시간: 2.94 ms
+메모리: 9.45 MB, 시간: 4.28 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 07일 11:19:04
+2026년 02월 07일 11:31:47
 
 ### 문제 설명
 
--- 프로그래머스/3/42898. 등굣길/등굣길.py
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -1,18 +1,21 @@
 def solution(m, n, puddles):
     MOD=1000000007
     inf=float("inf")
-    d=[[1]*(m+1) for _ in range(n+1)]
-    for i in range(1,m+1):
-        d[0][i]=0
-    for i in range(1,n+1):
-        d[i][0]=0
+    d=[[0]*(m+1) for _ in range(n+1)]
     d[1][0]=1
     for (pj,pi) in puddles:
-        d[pi][pj]=0
+        d[pi][pj]=-1
     for i in range(1,n+1):
         for j in range(1,m+1):
-            if d[i][j]!=0:
-                d[i][j]=(d[i-1][j]+d[i][j-1])%MOD
+            if d[i][j]!=-1 and not (d[i-1][j]==d[i][j-1]==-1):
+                if d[i-1][j]==-1:
+                    d[i][j]=(d[i][j-1])%MOD
+                elif d[i][j-1]==-1:
+                    d[i][j]=(d[i-1][j])%MOD
+                else:
+                    d[i][j]=(d[i-1][j]+d[i][j-1])%MOD
+                    
+                
     
     print(d)        
     answer = d[n][m]
