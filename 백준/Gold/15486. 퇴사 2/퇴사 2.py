fc7e0cd72c3c80cf542ba1134b2050992c952abe
XianXian <102227649+xianiax02@users.noreply.github.com>
2026-02-13 20:34:35 +0900

    [Gold V] Title: 퇴사 2, Time: 2240 ms, Memory: 348712 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.py
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -1,21 +1,14 @@
 import sys
-import heapq
 input=sys.stdin.readline
 n=int(input())
-tasklist=[[] for _ in range(1+n)] #끝나는 날 기준으로 저장하기 위해 0-padding
-maxval=[0]*(n+1)
-for s in range(1,n+1):
-    duration,w=map(int,input().split())
-    e=s+duration-1
-    if e<=n:
-        tasklist[e].append((s,w))
-
+c=[0]*(n+1) #0-padding
 for i in range(1,n+1):
-    candidates=[]
-    val=maxval[i-1] #오늘로 끝나는 사건을 고르지 않았을 때
-    for task in tasklist[i]:
-        val=max(val,task[1]+maxval[task[0]-1]) #i날에 끝나는 태스크를 고른것들에 대해서 가장 큰 값이 선정됨.
-    maxval[i]=val
-
-print(maxval[n])
+    c[i]=list(map(int,input().split()))
+dp=[0]*(n+2)
+for i in range(n,0,-1):
+    if i+c[i][0]-1>n: #enddate
+        dp[i]=max(0,dp[i+1])
+    else:
+        dp[i]=max(dp[i+c[i][0]]+c[i][1],dp[i+1])
     
+print(dp[1])
