4c2faa3f9b9abb4ead367dca25338b80be3c1dee
XianXian <102227649+xianiax02@users.noreply.github.com>
2026-02-14 14:20:29 +0900

    [level 2] Title: 기능개발, Time: 0.09 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,21 +1,15 @@
+import math
+from collections import deque
 def solution(progresses, speeds):
-    def gauss(a):
-        if a>int(a):
-            return int(a+1)
-        else:
-            return int(a)
     answer = []
     n=len(progresses)
-    r=[gauss((100-progresses[i])/speeds[i]) for i in range(n)]
-    
-    p1=0
-    p2=0
-    while p2<n:
-        cnt=0
-        while p2<n and r[p1]>=r[p2] :
-            cnt+=1
-            p2+=1
-        p1=p2
-        answer.append(cnt)
-        
+    daysleft=[math.ceil((100-progresses[i])/speeds[i]) for i in range(n)]
+    daysleft=deque(daysleft)
+    print(daysleft)
+    while daysleft:
+        c=daysleft.popleft()
+        answer.append(1)
+        while daysleft and daysleft[0]<=c:
+            daysleft.popleft()
+            answer[-1]+=1
     return answer
