aa13f64cf01245ef4095ce11f1b7c131bf585e7c
XianXian <102227649+xianiax02@users.noreply.github.com>
2026-02-14 15:16:27 +0900

    [level 3] Title: 단속카메라, Time: 0.98 ms, Memory: 9.71 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/README.md
+++ 프로그래머스/3/42884. 단속카메라/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단속카메라 - 42884 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42884#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42884) 
 
 ### 성능 요약
 
-메모리: 9.67 MB, 시간: 0.95 ms
+메모리: 9.71 MB, 시간: 0.98 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 05일 15:55:49
+2026년 02월 14일 15:16:27
 
 ### 문제 설명
 
--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -1,9 +1,12 @@
 def solution(routes):
     answer = 0
-    routes.sort(key=lambda x:x[1])
-    camera=-30001
+    routes.sort(key=lambda x: x[1])
+    cctv=-float('inf')
     for route in routes:
-        if camera<route[0]:
-            camera=route[1]
-            answer+=1 
+        if route[0]<=cctv<=route[1]:
+            continue
+        else:
+            cctv=route[1]
+            answer+=1
+    
     return answer
