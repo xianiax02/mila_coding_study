9f00e5ff71e0ad01d2ad5321b7aca9faf2e17cff
XianXian <102227649+xianiax02@users.noreply.github.com>
2026-02-14 20:16:59 +0900

    [Gold I] Title: 구간 합 구하기, Time: 972 ms, Memory: 120476 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.py
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.py
@@ -1,45 +1,42 @@
 import sys
 input=sys.stdin.readline
-leafn,m,k=map(int,input().split())
-treedepth=0 #leafn 이 1부터 주어짐
-while 2**treedepth<leafn:
-    treedepth+=1
-
-size=2**(treedepth+1)-1
-increment=2**treedepth-1 #index+increment=treeindex
-#전체 배열 크기 구하기
-tree=[0]*(size+1) #0은 부분합 구하든 뭘하든 영향이 없으니 
-for i in range(1,leafn+1):
-    a=int(input())
-    tree[i+increment]=a
-#세그먼트 트리 채우기    
-for i in range(size,0,-2):
-    tree[(i-1)//2]=tree[i]+tree[i-1]
+print=sys.stdout.write
+n,m,k=map(int,input().split())
+#이진트리이므로 leafnode에 n개 숫자를 모두 담을 수 있는 리프 크기를 구한다. 
+S=1
+while S<n:
+    S<<=1
+segtree=[0]*(2*S) #리프 노드 크기는 S 트리는 1~S
+#입력하기
+for i in range(S,S+n):
+    segtree[i]=int(input().strip())
+#채워넣기
+for i in range(S-1,0,-1):
+    segtree[i]=segtree[(i<<1)+1]+segtree[i<<1]
     
 for _ in range(m+k):
-    command,start,end=map(int,input().split())
-    if command==1:
-        diff=end-tree[start+increment]
-        tree[start+increment]=end
-        index=start+increment
-        while index>=1:
-            index=index//2 #go to parent
-            tree[index]+=diff        
-    elif command==2:
-        selected={}
+    command,a,b=map(int,input().split())
+    if command==1: #숫자 변경
+        idx=S+a-1
+        segtree[idx]=b
+        idx>>=1
+        while idx>0:
+            segtree[idx]=segtree[2*idx+1]+segtree[2*idx]
+            idx>>=1
+
+    elif command==2: #조회
         ans=0
-        start+=increment
-        end+=increment
-        while start<=end:#둘이 모이는 그 지점도 더해야함. 어차피 둘이 같을때 start==end 나머지는 0또는 1이니 둘중 하나에서만 더해지고 루프 끝남
-            if start%2==1 :#시작이 오른쪽 노드일때
-                ans+=tree[start]
-                start=(start+1)//2
-            else:
-                start=(start)//2
-            if end%2==0 : #끝이 왼쪽 노드일때
-                ans+=tree[end]
-                end=(end-1)//2
-            else:
-                end=(end//2)
-        print(ans)
-                
+        a=S+a-1
+        b=S+b-1
+        while a<=b:
+            if a%2==1: # a가 오른쪽 자식이면
+                ans+=segtree[a]
+                a+= 1
+            if b%2==0: # b가 왼쪽 자식이면
+                ans+=segtree[b]
+                b-=1
+            a>>=1
+            b>>=1
+        print(str(ans)+'\n')
+        
+        
