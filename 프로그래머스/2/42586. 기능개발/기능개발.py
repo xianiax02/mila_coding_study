4963120d141cddd8b54508c3808bd71052c7a1a3
XianXian <102227649+xianiax02@users.noreply.github.com>
2026-02-14 14:53:48 +0900

    [level 2] Title: 기능개발, Time: 0.03 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -3,13 +3,12 @@ from collections import deque
 def solution(progresses, speeds):
     answer = []
     n=len(progresses)
-    daysleft=[math.ceil((100-progresses[i])/speeds[i]) for i in range(n)]
-    daysleft=deque(daysleft)
-    print(daysleft)
-    while daysleft:
-        c=daysleft.popleft()
-        answer.append(1)
-        while daysleft and daysleft[0]<=c:
-            daysleft.popleft()
+    daysleft=[-(-(100-progresses[i])//speeds[i]) for i in range(n)]
+    maxday=-1
+    for i in daysleft:
+        if maxday>=i:
             answer[-1]+=1
+        else:
+            maxday=i
+            answer.append(1)
     return answer
