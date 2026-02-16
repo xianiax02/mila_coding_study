883a716f6aa05b6fcb632e4ad6e88ade09fa1de7
XianXian <102227649+xianiax02@users.noreply.github.com>
2026-02-16 14:19:08 +0900

    [Gold V] Title: 평범한 배낭, Time: 2164 ms, Memory: 44508 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,22 @@
+import sys
+input=sys.stdin.readline
+n,k=map(int,input().split())
+things=[]
+for _ in range(n):
+    things.append(list(map(int,input().split())))
+    
+d=dict()
+d[0]=0
+for (nw,nv) in things:
+    new_d=dict()
+    for w,v in d.items():
+        new_d[w]=v
+    for w,v in d.items():
+        if nw<=k:
+            new_d[nw]=max(nv,d.get(nw,0))
+        if w+nw<=k:
+            new_d[w+nw]=max(d.get(w+nw,0),d.get(w,0)+nv)
+    d=new_d
+
+answer=max([i for i in d.values()])
+print(answer)
