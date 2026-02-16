1e7377200e6aa51f3fecc9c306e50ce7b5eeb3fa
XianXian <102227649+xianiax02@users.noreply.github.com>
2026-02-16 11:20:30 +0900

    [Gold I] Title: 구간 합 구하기, Time: 716 ms, Memory: 120476 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.py
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.py
@@ -1,42 +1,40 @@
 import sys
 input=sys.stdin.readline
-print=sys.stdout.write
 n,m,k=map(int,input().split())
-#이진트리이므로 leafnode에 n개 숫자를 모두 담을 수 있는 리프 크기를 구한다. 
-S=1
-while S<n:
-    S<<=1
-segtree=[0]*(2*S) #리프 노드 크기는 S 트리는 1~S
-#입력하기
-for i in range(S,S+n):
-    segtree[i]=int(input().strip())
-#채워넣기
-for i in range(S-1,0,-1):
-    segtree[i]=segtree[(i<<1)+1]+segtree[i<<1]
+
+s=1
+while s<n:
+    s<<=1
+
+segtree=[0]*(2*s) #index: 1~2s-1
+for i in range(s,s+n):
+    segtree[i]=int(input())
+
+for i in range(s-1,0,-1):
+    segtree[i]=segtree[i<<1]+segtree[(i<<1)+1]
     
 for _ in range(m+k):
-    command,a,b=map(int,input().split())
-    if command==1: #숫자 변경
-        idx=S+a-1
-        segtree[idx]=b
-        idx>>=1
-        while idx>0:
-            segtree[idx]=segtree[2*idx+1]+segtree[2*idx]
-            idx>>=1
-
-    elif command==2: #조회
+    c,a,b=map(int,input().split())
+    if c==1:
+        a=s+a-1
+        segtree[a]=b
+        a>>=1
+        while a>0:
+            segtree[a]=segtree[a<<1]+segtree[(a<<1)+1]
+            a>>=1
+    
+    elif c==2:
+        a=s+a-1
+        b=s+b-1
         ans=0
-        a=S+a-1
-        b=S+b-1
         while a<=b:
-            if a%2==1: # a가 오른쪽 자식이면
+            if a%2==1:
                 ans+=segtree[a]
-                a+= 1
-            if b%2==0: # b가 왼쪽 자식이면
+                a+=1
+            if b%2==0:
                 ans+=segtree[b]
                 b-=1
             a>>=1
             b>>=1
-        print(str(ans)+'\n')
-        
-        
+        print(ans)
+                
