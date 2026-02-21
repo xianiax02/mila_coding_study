1d6c2ab44244d720eaba1283ab085b449fec92e2
XianXian <102227649+xianiax02@users.noreply.github.com>
2026-02-21 10:50:50 +0900

    [Gold III] Title: Dance Dance Revolution, Time: 748 ms, Memory: 33976 KB -BaekjoonHub

--- 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.py
+++ 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.py
@@ -1,33 +1,24 @@
 import sys
 input=sys.stdin.readline
-command=list(map(int,input().split()))
-del command[-1] #마지막 0은 삭제
-n=len(command)
-def getw(start,end):
-    if start==0:
-        return 2
-    elif start==end:
-        return 1
-    elif abs(start-end)==2:
-        return 4
-    else:
-        return 3
-
-dp={(0,0):0}
-for target in command:
-    new_dp={}
-    for (l,r),w in dp.items():
-        if target!=r: #왼쪽 움직일때
-            if (target,r) in new_dp:
-                new_dp[(target,r)]=min(new_dp[(target,r)],w+getw(l,target))
-            else:
-                new_dp[(target,r)]=w+getw(l,target)
-        if target!=l: #오른쪽 움직일때
-            if (l,target) in new_dp:
-                new_dp[(l,target)]=min(new_dp[(l,target)],w+getw(r,target))
-            else:
-                new_dp[(l,target)]=w+getw(r,target)
-        
-    dp=new_dp
-
-print(min(dp.values()))
+commands=list(map(int,input().split()))
+del commands[-1]
+d={(0,0):0}
+cost=[[1,2,2,2,2],
+      [0,1,3,4,3],
+      [0,3,1,3,4],
+      [0,4,3,1,3],
+      [0,3,4,3,1]]
+inf=float('inf')
+for step in commands:
+    new_d=dict()
+    for state,v in d.items():
+       l=state[0]
+       r=state[1]
+       if step in state:
+           new_d[state]=min(v+1,new_d.get(state,inf))
+       else:
+           new_d[(step,r)]=min(v+cost[l][step],new_d.get((step,r),inf))
+           new_d[(l,step)]=min(v+cost[r][step],new_d.get((l,step),inf))
+    d=new_d
+answer=min(list(d.values()))
+print(answer)
