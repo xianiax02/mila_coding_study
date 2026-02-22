8f889a5b64e6a8f6a4612aeca7b350ff2626759a
XianXian <102227649+xianiax02@users.noreply.github.com>
2026-02-22 12:33:24 +0900

    [level 5] Title: 시험장 나누기, Time: 189.57 ms, Memory: 21.2 MB -BaekjoonHub

--- 프로그래머스/5/81305. 시험장 나누기/시험장 나누기.py
+++ 프로그래머스/5/81305. 시험장 나누기/시험장 나누기.py
@@ -1,54 +1,46 @@
 import sys
 sys.setrecursionlimit(10**6)
-def find_root(n, links,parent):
-    for i in range(n):
-        if links[i][0]!=-1:
-            parent[links[i][0]]=i
-        if links[i][1]!=-1:
-            parent[links[i][1]]=i
-    root_index=parent.index(-1)
-    return root_index
-
-
-            
-
 def solution(k, num, links):
-    answer = float('inf')
+    answer = 0
+    cnt=0
     n=len(num)
-    parent=[-1]*(n)
-    root=find_root(n,links,parent)
-    def postorder(v,L,groupsize):
-        nonlocal cnt
-        r_sum,l_sum=0,0    
-        if links[v][0]==-1 and links[v][1]==-1:
-            groupsize[v]=num[v]
-            return
-        if links[v][0]!=-1:
-            postorder(links[v][0],L,groupsize)
-            l_sum=groupsize[links[v][0]]
-        if links[v][1]!=-1:
-            postorder(links[v][1],L,groupsize)
-            r_sum=groupsize[links[v][1]]
-        if l_sum+r_sum+num[v]>L:
-            if min(l_sum,r_sum)+num[v]<=L:
-                groupsize[v]=min(l_sum,r_sum)+num[v]
-                cnt+=1
-            else:
-                groupsize[v]=num[v]
-                cnt+=2
-        else:
-            groupsize[v]=l_sum+r_sum+num[v]
-            
-    end=sum(num)
-    start=max(num)
-    while start<=end:
-        L=(start+end)//2
+    is_child=[False]*n
+    for l,r in links:
+        if l!=-1: is_child[l]=True
+        if r!=-1: is_child[r]=True
+    root=is_child.index(False)
+    
+    def is_ok(maxnum):
         cnt=0
-        groupsize=[0]*(n)
-        postorder(root,L,groupsize)
-        if cnt<k:
-            answer=min(answer,max(groupsize))
-            end=L-1
+        def postorder(node,maxnum):
+            nonlocal cnt
+            l,r=links[node][0],links[node][1]
+            l_sum=postorder(l,maxnum) if l!=-1 else 0
+            r_sum=postorder(r,maxnum) if r!=-1 else 0
+            if l_sum+r_sum+num[node]<=maxnum:
+                return l_sum+r_sum+num[node]
+            elif l_sum>=r_sum and r_sum+num[node]<=maxnum: #오른쪽 자식을 자른다
+                cnt+=1
+                return r_sum+num[node]
+            elif l_sum<r_sum and l_sum+num[node]<=maxnum:
+                cnt+=1
+                return l_sum+num[node]
+            else:
+                cnt+=2
+                return num[node]
+        
+        postorder(root,maxnum)
+        if cnt>=k:
+            return False
+        else: return True
+    
+    start,end=max(num),sum(num)
+    while start<=end:
+        mid=(start+end)//2
+        if is_ok(mid):
+            end=mid-1
         else:
-            start=L+1
+            start=mid+1
+    answer=start
+
     return answer
