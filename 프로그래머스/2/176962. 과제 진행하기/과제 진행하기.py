6fbe3953512c88c761eb8d95c985c3ac3c045e9b
XianXian <102227649+xianiax02@users.noreply.github.com>
2026-02-23 18:57:41 +0900

    [level 2] Title: 과제 진행하기, Time: 4.23 ms, Memory: 9.74 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
@@ -10,25 +10,30 @@ def solution(plans):
     for i,plan in enumerate(plans):
         time=convert(plan[1])
         heapq.heappush(mq,(convert(list(plan[1].split(':'))),int(plan[2]),plan[0])) #시작시간, 소요시간, 과목이름
+        
     prevtime,prevname=0,''
     while mq:
-        if prevtime>mq[0][0]: #앞일이 끝나는 시간이 지금 시작하려는 시간보다 느릴때 우선 stoplist에 넣는다
-            stoplist.append((mq[0][0],prevtime-mq[0][0],prevname))
-        elif stoplist and prevtime<mq[0][0]: #앞일이 일찍 끝나고 중지시킨 일이 있을때 중지시킨 일을 집어넣는다.
-            while stoplist and prevtime<mq[0][0]:
-                answer.append(prevname)
-                _,spendtime,name=stoplist.pop()
-                prevtime+=spendtime
-                prevname=name
-            continue
-        else: #딱맞게 끝날때 + 일찍 끝나고, stoplist가 없을때 완료 표시후, mq 빼면서 값들 갱신
-            if prevname:
-                answer.append(prevname)
         starttime,spendtime,name=heapq.heappop(mq)
-        prevtime,prevname=starttime+spendtime,name
+        if prevname:
+            if prevtime>starttime: #여유 없이 중단하고 다른거 시작해야하는 경우
+                stoplist.append((prevtime-starttime,prevname))
+            else:
+                answer.append(prevname)
+                gap=starttime-prevtime #다음 요청때까지 남은 시간 이 존재하는 동안, stoplist에서 최대한 빼오기
+                while gap and stoplist:
+                    st,na=stoplist.pop()
+                    if gap>=st:
+                        gap-=st
+                        answer.append(na)
+                    else:
+                        st-=gap
+                        gap=0
+                        stoplist.append((st,na))
+        prevtime, prevname = starttime + spendtime, name
+        
     answer.append(prevname) #마지막 탈출 이후 저장해주기.
     while stoplist:
-        _,spendtime,name=stoplist.pop()
+        spendtime,name=stoplist.pop()
         answer.append(name)
     
     return answer
