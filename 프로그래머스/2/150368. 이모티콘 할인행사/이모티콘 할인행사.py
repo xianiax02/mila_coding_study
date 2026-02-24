708f1da13d47c0dc3988ed4bbeff4a90740495d7
XianXian <102227649+xianiax02@users.noreply.github.com>
2026-02-24 14:43:23 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 1148.81 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
@@ -1,3 +1,24 @@
+from itertools import product
 def solution(users, emoticons):
-    answer = []
+    emon=len(emoticons)
+    cases=list(product([10,20,30,40],repeat=emon))
+    def getresult(case):
+        totalsales=0
+        totalmembers=0
+        prices=[int((100-rate)*(price//100)) for rate,price in zip(case,emoticons)]
+        for userrate,useramount in users:
+            usersales=0
+            for i in range(emon):
+                if case[i]>=userrate:
+                    usersales+=prices[i]
+                if usersales>=useramount:
+                    break
+            if usersales>=useramount:
+                totalmembers+=1
+            else:
+                totalsales+=usersales
+        return (totalmembers,totalsales)
+    answer=[0,0]
+    for case in cases:
+        answer=max(answer,list(getresult(case)))
     return answer
