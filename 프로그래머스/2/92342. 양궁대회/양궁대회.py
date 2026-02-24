104ea85990a46e81735997ae3d7c43bde1fed736
XianXian <102227649+xianiax02@users.noreply.github.com>
2026-02-24 18:46:01 +0900

    [level 2] Title: 양궁대회, Time: 1004.83 ms, Memory: 64 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.py
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.py
@@ -1,3 +1,34 @@
+from collections import Counter
+from itertools import combinations_with_replacement
 def solution(n, info):
-    answer = []
+    cases=list(combinations_with_replacement([i for i in range(11)],n))
+    def convert(case):
+        lst=[0]*11
+        counter=Counter(case)
+        for i in range(11):
+            lst[i]=counter[i]
+        return lst
+    cases=[convert(case) for case in cases]
+    casenum=len(cases)
+    casescores=[0]*casenum
+    def getscorediff(ryan):
+        peachscore=0
+        ryanscore=0
+        for i in range(11):
+            if info[i]>=ryan[i]:
+                if info[i]!=0:
+                    peachscore+=10-i
+            else:
+                ryanscore+=10-i
+        return ryanscore-peachscore
+    maxscorediff=(0,[])
+    cands=[]
+    for i,case in enumerate(cases):
+        scorediff=getscorediff(case)
+        casescores[i]=scorediff
+        if (scorediff,case[::-1])>maxscorediff:
+            maxscorediff=(scorediff,case[::-1])
+            answer=case
+    if maxscorediff[0]<=0:
+        return [-1] 
     return answer
