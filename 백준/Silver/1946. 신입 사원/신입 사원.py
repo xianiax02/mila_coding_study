912304ab85c60e56128076af3b3f27ef53e2b38b
XianXian <102227649+xianiax02@users.noreply.github.com>
2026-02-25 18:22:06 +0900

    [Silver I] Title: 신입 사원, Time: 5344 ms, Memory: 57288 KB -BaekjoonHub

---
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -0,0 +1,18 @@
+import sys
+input=sys.stdin.readline
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    lst=[]
+    cnt=0
+    for _ in range(n):
+        lst.append(tuple(map(int,input().split())))
+    lst.sort(key=lambda x:(x[0],-x[1]))
+    maxs1,mins2=0,float('inf')
+    for i in range(len(lst)):
+        if lst[i][0]>maxs1 and lst[i][1]>mins2:
+           cnt+=1
+        else:
+            maxs1=max(lst[i][0],maxs1)
+            mins2=min(lst[i][1],mins2)
+    print(len(lst)-cnt)
