ff61db71f829b3481bc32e8fc0f0c119ac2e54b1
XianXian <102227649+xianiax02@users.noreply.github.com>
2026-02-26 14:50:13 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 1.06 ms, Memory: 9.38 MB -BaekjoonHub

---
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.py
@@ -0,0 +1,21 @@
+def solution(n, t, m, timetable):
+    convert= lambda x : int(x[:2])*60+int(x[3:])
+    timetable=[convert(time) for time in timetable]
+    timetable.sort()
+    bustime,buscnt,pos=9*60,1,0
+    print(timetable)
+    while buscnt<=n:
+        inthisbus=0
+        while inthisbus<m and pos<len(timetable) and timetable[pos]<=bustime:
+            pos+=1
+            inthisbus+=1
+        if buscnt==n:
+            if inthisbus<m: #마지막 차가 다 안찼다면, 아무도 안탄것 포함
+                answer=bustime
+            else:
+                answer=timetable[pos-1]-1
+        print(bustime,inthisbus,buscnt,pos)
+        bustime+=t
+        buscnt+=1
+    answer = f'{answer//60:02d}:{answer%60:02d}'
+    return answer
