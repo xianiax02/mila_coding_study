b42ece11726ccbb7e7c943fec83f09aee07939c6
XianXian <102227649+xianiax02@users.noreply.github.com>
2026-02-26 21:34:52 +0900

    [Silver II] Title: DFS와 BFS, Time: 64 ms, Memory: 35424 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,40 +1,35 @@
 import sys
 from collections import deque
-N, M, V = map(int, sys.stdin.readline().split())
-graph = [[] for _ in range(N + 1)]
-
-for _ in range(M):
-    a, b = map(int, sys.stdin.readline().split())
-    graph[a].append(b)
-    graph[b].append(a)
-
-
-for i in range(1, N + 1):
-    graph[i].sort()
-
-def dfs(start_node):
-    visited_dfs[start_node] = True
-    print(start_node, end=' ')
-    for neighbor in graph[start_node]:
-        if not visited_dfs[neighbor]:
-            dfs(neighbor)
-
-def bfs(start_node):
-    queue = deque([start_node])
-    visited_bfs[start_node] = True
-    
-    while queue:
-        current_node = queue.popleft()
-        print(current_node, end=' ')
-        
-        for neighbor in graph[current_node]:
-            if not visited_bfs[neighbor]:
-                queue.append(neighbor)
-                visited_bfs[neighbor] = True
-
-visited_dfs = [False] * (N + 1)
-visited_bfs = [False] * (N + 1)
-
-dfs(V)
-print()
-bfs(V)
+input=sys.stdin.readline
+mq=deque()
+n,m,v=map(int,input().split())
+adlist=[[] for _ in range(n+1)]
+visited=[False]*(n+1)
+for _ in range(m):
+    a,b=map(int,input().split())
+    adlist[a].append(b)
+    adlist[b].append(a)
+for i in range(n+1):
+    adlist[i].sort()
+bfs=[v]
+dfs=[]
+mq.append(v)
+visited[v]=True
+while mq:
+    c=mq.popleft()
+    for nxt in adlist[c]:
+        if not visited[nxt]:
+            mq.append(nxt)
+            visited[nxt]=True
+            bfs.append(nxt)
+visited=[False]*(n+1)
+visited[v]=True
+def dfsf(v):
+    dfs.append(v)
+    for nxt in adlist[v]:
+        if not visited[nxt]:
+            visited[nxt]=True
+            dfsf(nxt)
+dfsf(v)
+print(*dfs)
+print(*bfs)
