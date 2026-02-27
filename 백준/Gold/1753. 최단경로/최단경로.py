6f65b602a43e228c0ec26bf0188565b895172fc7
XianXian <102227649+xianiax02@users.noreply.github.com>
2026-02-27 21:31:37 +0900

    [Gold IV] Title: 최단경로, Time: 664 ms, Memory: 69516 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,31 +1,29 @@
-import sys
 import heapq
+import sys
 input=sys.stdin.readline
-inf=float('inf')
 v,e=map(int,input().split())
 start=int(input())
-graph=[[] for _ in range(v+1)] #0-padding
+inf=float('inf')
+dist=[inf]*(v+1)
+adlist=[[] for _ in range(v+1)]
 for _ in range(e):
     a,b,w=map(int,input().split())
-    graph[a].append((b,w))
-heap=[]#0-padding 힙을 쓰기 위해 노드 정보도 포함시킴
-heapq.heappush(heap,(0,start))
-distance=[inf]*(1+v)
-distance[start]=0
-#visited=[False]*(1+v)
-#visited[0]=True #visited가 사실상 거리가 inf 냐 아니냐로 판단이 가능.
-while heap: #visited를순차적으로 배열하지 않기 위해 존재여부로 while 문 생성 -> 하나씩 없어지는 자료구조를 활용해야함.
-    mindist,minnode=heapq.heappop(heap)
-    if distance[minnode]>= mindist: #지금 탐색하는게 더 짧은 거리일 경우 
-        for nextnode,w in graph[minnode]:
-            temp=distance[nextnode]
-            distance[nextnode]=min(distance[minnode]+w,distance[nextnode])
-            if distance[nextnode]!=temp:
-                heapq.heappush(heap,(distance[nextnode],nextnode))
+    adlist[a].append((b,w))
+dist[start]=0
+mq=[(0,start)]
+while mq:
+    d,node=heapq.heappop(mq)
+    if d<=dist[node]:
+        for nxtnode,nxtdist in adlist[node]:
+            if dist[node]+nxtdist<dist[nxtnode]:
+                dist[nxtnode]=dist[node]+nxtdist
+                heapq.heappush(mq,(dist[node]+nxtdist,nxtnode))
 
-for dist in range(1,v+1):
-    if distance[dist]==inf:
+for d in dist[1:]:
+    if d==inf:
         print('INF')
     else:
-        print(distance[dist])
-
+        print(d)
+            
+    
+    
