4159b5abf6cf086aaac6746f0bc4052308808a89
XianXian <102227649+xianiax02@users.noreply.github.com>
2026-03-01 21:38:29 +0900

    [Gold IV] Title: LCS, Time: 432 ms, Memory: 56604 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,14 +1,11 @@
-import sys
-input=sys.stdin.readline
-a=input().strip()
-b=input().strip()
-d=[[0]*(len(b)+1) for _ in range(len(a)+1)]
+a=input()
+b=input()
+dp=[[0]*(1+len(b)) for _ in range(1+len(a))]
 for i in range(1,len(a)+1):
     for j in range(1,len(b)+1):
         if a[i-1]==b[j-1]:
-            d[i][j]=d[i-1][j-1]+1
+            dp[i][j]=dp[i-1][j-1]+1
         else:
-            d[i][j]=max(d[i-1][j],d[i][j-1])
-            
-            
-print(d[-1][-1])
+            dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+
+print(dp[len(a)][len(b)])
