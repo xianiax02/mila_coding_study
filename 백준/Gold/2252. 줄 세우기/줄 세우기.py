bd7752d4add708b2bf7e42b54c6ff78fd9229d0f
XianXian <102227649+xianiax02@users.noreply.github.com>
2026-03-01 16:33:27 +0900

    [Gold III] Title: 줄 세우기, Time: 200 ms, Memory: 40912 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -1,34 +1,28 @@
-import sys
 from collections import deque
-mq=deque()
+import sys
 input=sys.stdin.readline
 n,m=map(int,input().split())
-graph=[[]for _ in range(n+1)] #0-padding
-indegree=[0]*(n+1) #0padding
-indegree[0]=-1
-result=[]
-#visited=[False]*(n+1)
-#visited[0]=True
-
+indegree=[0]*(n+1)
+mq=deque()
+adlist=[[] for _ in range(n+1)]
 for _ in range(m):
     a,b=map(int,input().split())
-    graph[a].append(b)
+    adlist[a].append(b)
     indegree[b]+=1
-
+answer=[]
 for i in range(1,n+1):
-    if indegree[i]==0:
-        mq.append(i)
-    
-while mq:
-    currentnode=mq.popleft()    
-    result.append(currentnode)
-    #visited[currentnode]=True
-    for nextnode in graph[currentnode]:
-        indegree[nextnode]-=1
-        if indegree[nextnode]==0 : #0진입차수가 0이 된다는 것은 곳 이 노드를 가리키는 노드가 더 이상 없다는 뜻이므로 중복으로 방문여지 x
-            mq.append(nextnode)
-                    
+    if not indegree[i]:
+       mq.append(i)
 
-print(*result)
-                
-                
+while mq:
+    c=mq.popleft()
+    answer.append(c)
+    for nxt in adlist[c]:
+        indegree[nxt]-=1
+        if indegree[nxt]==0:
+            mq.append(nxt)
+            
+if len(answer)!=n:
+    print('impossible')
+else:
+    print(*answer)
