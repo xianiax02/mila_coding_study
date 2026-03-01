7a628877e914e5b777370ebeed29e56d0bf99193
XianXian <102227649+xianiax02@users.noreply.github.com>
2026-03-01 13:06:36 +0900

    [Gold IV] Title: 최단경로, Time: 628 ms, Memory: 69516 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,29 +1,28 @@
-import heapq
 import sys
+import heapq
 input=sys.stdin.readline
 v,e=map(int,input().split())
 start=int(input())
-inf=float('inf')
-dist=[inf]*(v+1)
 adlist=[[] for _ in range(v+1)]
 for _ in range(e):
     a,b,w=map(int,input().split())
     adlist[a].append((b,w))
-dist[start]=0
-mq=[(0,start)]
+mq=[]
+inf=float('inf')
+distance=[inf]*(v+1)
+distance[start]=0
+heapq.heappush(mq,(0,start))
 while mq:
-    d,node=heapq.heappop(mq)
-    if d<=dist[node]:
-        for nxtnode,nxtdist in adlist[node]:
-            if dist[node]+nxtdist<dist[nxtnode]:
-                dist[nxtnode]=dist[node]+nxtdist
-                heapq.heappush(mq,(dist[node]+nxtdist,nxtnode))
-
-for d in dist[1:]:
-    if d==inf:
+    cd,cn=heapq.heappop(mq)
+    if cd<=distance[cn]:
+        distance[cn]=cd
+        for nxt,nw in adlist[cn]:
+            if cd+nw<distance[nxt]:
+                distance[nxt]=cd+nw
+                heapq.heappush(mq,(cd+nw,nxt))
+for i in range(1,len(distance)):
+    dist=distance[i]
+    if dist==inf:
         print('INF')
     else:
-        print(d)
-            
-    
-    
+        print(dist)
