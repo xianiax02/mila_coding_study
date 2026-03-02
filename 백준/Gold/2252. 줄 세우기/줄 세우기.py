0d72215c55bb6ef53b8b61aa1b7112f8895da162
XianXian <102227649+xianiax02@users.noreply.github.com>
2026-03-02 15:11:48 +0900

    [Gold III] Title: 줄 세우기, Time: 184 ms, Memory: 40704 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -2,18 +2,18 @@ from collections import deque
 import sys
 input=sys.stdin.readline
 n,m=map(int,input().split())
-indegree=[0]*(n+1)
-mq=deque()
-adlist=[[] for _ in range(n+1)]
+indegree=[0]*(1+n)
+adlist=[[] for _ in range(1+n)]
 for _ in range(m):
     a,b=map(int,input().split())
     adlist[a].append(b)
     indegree[b]+=1
-answer=[]
-for i in range(1,n+1):
-    if not indegree[i]:
-       mq.append(i)
 
+mq=deque()
+for i in range(1,n+1):
+    if indegree[i]==0:
+        mq.append(i)
+answer=[]
 while mq:
     c=mq.popleft()
     answer.append(c)
@@ -21,8 +21,5 @@ while mq:
         indegree[nxt]-=1
         if indegree[nxt]==0:
             mq.append(nxt)
-            
-if len(answer)!=n:
-    print('impossible')
-else:
-    print(*answer)
+
+print(*answer)
