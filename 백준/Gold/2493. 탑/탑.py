6e32381a58ba757dc64896dfafb180103b3ae3bb
XianXian <102227649+xianiax02@users.noreply.github.com>
2026-03-03 15:03:59 +0900

    [Gold V] Title: 탑, Time: 624 ms, Memory: 108180 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -4,7 +4,8 @@ towers=list(map(int,input().split()))
 stack=[]
 towers=[(num,i) for i,num in enumerate(towers)]
 answer=[0]*n
-for height,idx in towers[::-1]:
+while towers:
+    height,idx=towers.pop()
     while stack and stack[-1][0]<height:
         h,i=stack.pop()
         answer[i]=idx+1
