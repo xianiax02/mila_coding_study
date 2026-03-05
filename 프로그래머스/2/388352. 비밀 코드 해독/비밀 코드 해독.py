ebc5b08b5bbfedd594ccb5853aaf51bbe6ffffc7
XianXian <102227649+xianiax02@users.noreply.github.com>
2026-03-05 20:35:48 +0900

    [level 2] Title: 비밀 코드 해독, Time: 225.15 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.py
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.py
@@ -1,8 +1,8 @@
 from itertools import combinations
 def solution(n, q, ans):
     answer = 0
-    candidates=[set(comb) for comb in combinations(range(1,n+1),5)]
-    for candidate in candidates:
+    for comb in combinations(range(1,n+1),5):
+        candidate=set(comb)
         thisisok=True
         for query,response in zip(q,ans):
             query=set(query)
